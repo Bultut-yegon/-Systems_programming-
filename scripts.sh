7da41326ed6b545eb3bd2731343c362813269bbb
Bultut-yegon <bultut9785@gmail.com>
2024-06-26 17:28:39 +0300

    java socket programming

---
+++ scripts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+touch distributed_systems.txt
+
+
+
+echo "compare and contrast the architectural design of WinXP and Unix in terms of :
+General issues
+Layering and their functions
+Handling of multiple tasks, multiuser applications, multiprocessing
+
+
+Stability of and security of processes in memory ">distributed_system.txt
+
+
+pwd
+
+mkdir cmd
+cd cmd
+
+
