47fe51844bb34b56e9b1890cf31e0d4eb8b87fab
Bultut-yegon <bultut9785@gmail.com>
2024-06-28 12:33:22 +0300

    stringFunctions

---
+++ stringFunctions.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#include<string.h>
+void main(){
+char dest[100]="Gen Z generation";
+
+
+
+char src[100]="is always amazing";
+
+
+strcat(dest,src);
+
+printf("%s",&dest);
+}
+
