54f9bf946918ef0d7a6bfaed6a39fbd09c07ff27
Bultut-yegon <bultut9785@gmail.com>
2024-07-22 13:20:52 +0300

    cat1

--- Cat1/quiz_20.c
+++ Cat1/quiz_20.c
@@ -9,12 +9,12 @@ int main() {
     if (isalpha(ch)) { // Check if the character is an alphabet
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
             ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-            printf("%c is a vowel.\n", ch);
+            printf("%c is a vowel.\n", ch);//if character is even
         } else {
             printf("%c is a consonant.\n", ch);
         }
     } else {
-        printf("%c is not an alphabet.\n", ch);
+        printf("%c is not an alphabet.\n", ch);//if the character is not an alphabet
     }
 
     return 0;
