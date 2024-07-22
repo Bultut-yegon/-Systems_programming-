6f9607268e8708be3d3d4e277987ee48fad3b1b6
Bultut-yegon <bultut9785@gmail.com>
2024-07-22 18:20:38 +0300

    pointers application-min-max

--- Arrays-pointers/min-max-array.c
+++ Arrays-pointers/min-max-array.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-int a[]={1,2,3,4,5,6,7,8,9,10},min=a[0],max=a[0],i;
-for(i=1;i<10;i++){
-    if(a[i]<min){
-        min=a[i];
-    }
-    else if(a[i]>max){
-        max=a[i];
+
+void minMaxValues(int arr[],int len,int *min,int *max){
+    *min = *max=arr[0];
+    for(int i=1;i<len;i++){
+        if(arr[i]<*min)
+        *min=arr[i];
+        if(arr[i]>*max)
+        *max=arr[i];
     }
 }
+int main(){
+int array[]={17,45,22,13,56,78,90,56,392},min,max,i,len=sizeof(array)/sizeof(array[0]);
+
+minMaxValues(array,len,&min,&max);
+printf("Minimum value in the array is: %d\n and maximum value is: %d",min,max);
 return 0;
 
 }
