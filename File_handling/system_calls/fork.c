905caba1b19bc1da76e8a2cd8a4fd37812d97f0c
Bultut-yegon <bultut9785@gmail.com>
2024-07-26 09:49:53 +0300

    getpid,getppid

--- File_handling/system_calls/fork.c
+++ File_handling/system_calls/fork.c
@@ -3,16 +3,17 @@
 int main(){
     int id=fork();
     printf("The  process\n");
-    // if(id==0){
-    //     printf("The child process\n");}
-    // else{
-    //     printf("The main process\n");
+    if(id==0){
+        printf("The child process\n");}
+    else{
+        printf("The main process\n");
 
-    //     }
+        }
 
     if(id!=0){
         fork();
     }
+    printf("Child process: %d\n Parent process: %d\n",getpid(),getppid()); 
     
     return 0;
 }
