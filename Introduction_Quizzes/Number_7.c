5aa27e11c6ae2834f0226221b03660e7c19516a7
Bultut-yegon <bultut9785@gmail.com>
2024-07-26 12:29:37 +0300

    -del

--- Introduction_Quizzes/Number_7.c
+++ Introduction_Quizzes/Number_7.c
@@ -1,28 +1,29 @@
-/* This code has a compile-time error, and at
-** least one run-time error. */
 #include <stdio.h>
 #include <math.h>
-main(int argc,char *argv[])
-{
-    int n,i;
-    int d2,count, double d1;
-    while (1)
-    {
+
+int main(int argc, char *argv[]) {
+    int n, i;
+    int count;
+
+    while (1) {
         printf("Enter a number (0 to quit): ");
-        scanf("%d",&n);
+        scanf("%d", &n);
         if (n == 0)
             break;
-        count=0;
-        for (i=0; i<n; i++)
-        {
-            d1=(double)n/(double)i;
-            d2=n/i;
-            if (fabs(d1-(double)d2) < 0.00001)
+        
+        count = 0;
+        for (i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
                 count++;
+                break;
+            }
         }
-        if (count == 2)
-            printf("%d is prime\n",n);
+        
+        if (count == 0 && n != 1)
+            printf("%d is prime\n", n);
         else
-            printf("%d is not prime\n",n);
+            printf("%d is not prime\n", n);
     }
+
+    return 0;
 }
